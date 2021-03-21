bb04b70ccf3c8880afd26464ca0fc8f905c2b69d
mateusz.glowacki <mateusz.glowacki@globallogic.com>
2021-03-21 22:38:11 +0100

    Zmieniono styl wyboru typu danych na nie- edytowalny

--- Data_Generator/GenerateFieldControl.Designer.cs
+++ Data_Generator/GenerateFieldControl.Designer.cs
@@ -54,6 +54,7 @@ namespace Data_Generator
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(98, 6);
             this.comboBox1.Name = "comboBox1";
